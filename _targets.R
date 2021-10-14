2f51f16a852062b1053f31d576ae1d9e2ce2b8b6
Galen Gorski <32860978+galengorski@users.noreply.github.com>
2021-10-14 11:12:06 -0400

    refactor pipeline to make more intuitive

--- _targets.R
+++ _targets.R
@@ -5,19 +5,55 @@ source("3_visualize/src/plot_timeseries.R")
 
 options(tidyverse.quiet = TRUE)
 tar_option_set(packages = c("tidyverse", "dataRetrieval")) # Loading tidyverse because we need dplyr, ggplot2, readr, stringr, and purrr
-
+#site numbers
+sites = c("01427207", "01432160","01435000", "01436690", "01466500")
+#parameter codes for query
+parameterCd = '00010' 
+startDate="2014-05-01" 
+endDate="2015-05-01"
+#parameters for plotting
 p_width <- 12
 p_height <- 7
 p_units <- "in"
 
 p1_targets_list <- list(
   tar_target(
-    site_data,
-    download_nwis_data(),
+    create_directories,
+    create_dirs(sites),
+    format = "file"
+  ),
+  tar_target(
+    site_data_1,
+    download_nwis_site_data(sites[1], create_directories,parameterCd, startDate, endDate),
+    format = "file"
+  ),
+  tar_target(
+    site_data_2,
+    download_nwis_site_data(sites[2], create_directories,parameterCd, startDate=, endDate=),
+    format = "file"
+  ),
+  tar_target(
+    site_data_3,
+    download_nwis_site_data(sites[3], create_directories,parameterCd, startDate=, endDate=),
+    format = "file"
+  ),
+  tar_target(
+    site_data_4,
+    download_nwis_site_data(sites[4], create_directories,parameterCd, startDate=, endDate=),
+    format = "file"
+  ),
+  tar_target(
+    site_data_5,
+    download_nwis_site_data(sites[5], create_directories,parameterCd, startDate=, endDate=),
+    format = "file"
+  ),
+  tar_target(
+    site_data_concat,
+    concat_files_to_df()
   ),
   tar_target(
     site_info_csv,
-    nwis_site_info(fileout = "1_fetch/out/site_info.csv", site_data),
+    nwis_site_info(fileout = '1_fetch/out', sites = sites),
     format = "file"
   )
 )
@@ -25,7 +61,7 @@ p1_targets_list <- list(
 p2_targets_list <- list(
   tar_target(
     site_data_clean, 
-    process_data(site_data)
+    process_data(site_data_concat)
   ),
   tar_target(
     site_data_annotated,
