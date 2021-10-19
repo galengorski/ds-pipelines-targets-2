47101f6109e314b78cff21e4122715c2cb8035bf
Galen Gorski <32860978+galengorski@users.noreply.github.com>
2021-10-19 13:34:16 -0400

    add phases as p_ to each target to follow USGS conventions

--- 2_process.R
+++ 2_process.R
@@ -2,15 +2,15 @@ source("2_process/src/process_and_style.R")
 
 p2_targets_list <- list(
   tar_target(
-    site_data_clean, 
-    process_data(site_data_concat)
+    p2_site_data_clean, 
+    process_data(p1_site_data_concat)
   ),
   tar_target(
-    site_data_annotated,
-    annotate_data(site_data_clean, site_filename = site_info_csv)
+    p2_site_data_annotated,
+    annotate_data(p2_site_data_clean, site_filename = p1_site_info_csv)
   ),
   tar_target(
-    site_data_styled,
-    style_data(site_data_annotated)
+    p2_site_data_styled,
+    style_data(p2_site_data_annotated)
   )
 )
